1ff3a3d929dbbdb74bb5dd6d7957c73114332ffe
Yoav <yoav.meron11@gmail.com>
2021-12-29 18:04:38 +0200

    Added HeadMasterCommsResource.

--- src/main/java/org/WikiRelations/WikiRelationsAgent.java
+++ src/main/java/org/WikiRelations/WikiRelationsAgent.java
@@ -3,8 +3,12 @@ package org.WikiRelations;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.WikiRelations.HeadMasterCommunications.HeadMasterCommunicationHandler;
 import org.WikiRelations.core.LinkThreadList;
+import org.WikiRelations.resources.HeadMasterCommsResource;
 import org.WikiRelations.resources.LinksResource;
+
+import java.io.IOException;
 
 public class WikiRelationsAgent extends Application<WikiRelationsAgentConfiguration> {
 
@@ -24,11 +28,13 @@ public class WikiRelationsAgent extends Application<WikiRelationsAgentConfigurat
 
     @Override
     public void run(final WikiRelationsAgentConfiguration configuration,
-                    final Environment environment) {
+                    final Environment environment) throws IOException, InterruptedException {
         // TODO: implement application
         LinkThreadList linkThreadList = new LinkThreadList();
+        HeadMasterCommunicationHandler headMasterCommunicationHandler = new HeadMasterCommunicationHandler(linkThreadList);
+        headMasterCommunicationHandler.sendHeadMasterAgentInfo("8090");
         environment.jersey().register(new LinksResource(linkThreadList));
-
+        environment.jersey().register(new HeadMasterCommsResource(linkThreadList));
 
     }
 
